ba6ffd9c3957146d6d418e622f8e2d2d23d723ed
Robiul Hossain Raju <58886154+RHRaju01@users.noreply.github.com>
2024-08-25 21:51:29 +0600

    Delete scripts/script.js

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const menu = document.querySelector(".navigation");
-const nav = document.querySelector(".nav");
-const navClose = document.querySelector(".close-menu");
-const links = document.querySelector(".links");
